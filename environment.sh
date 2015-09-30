df6c0aba3d2332a5b05e9cc8a5a7dd602d725906
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-09-30 16:37:34 +0100

    use preview domain
    We're migrating from "beta" to "preview" in the domains to better
    reflect the status of the server rather than the status of the project.
    See also openregister/deployment#31

--- environment.sh
+++ environment.sh
@@ -1,4 +1,4 @@
 export SETTINGS='config.DevelopmentConfig'
-export PREMISES_REGISTER='http://premises.beta.openregister.org'
+export PREMISES_REGISTER='http://premises.preview.openregister.org'
 export MONGO_URI='mongodb://localhost:27017'
 export COMPANIES_HOUSE_API_KEY='not-real'
